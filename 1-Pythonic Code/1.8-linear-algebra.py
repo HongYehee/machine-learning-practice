ddefbffdbc0f0d5ceb49ab7c5dc67d2bfba9c593
yh.hong <ghddpgml@gmail.com>
2024-03-18 18:55:08 +0900

    1.8

--- 1-Pythonic Code/1.8-linear-algebra.py
+++ 1-Pythonic Code/1.8-linear-algebra.py
@@ -1,7 +1,20 @@
 # Linear Algebra Codes
 
-# Vector 표현
+# Vector representation
 vector_a = [1, 2, 10] # list
 vector_b = (1, 2, 10) # tuple
 vector_c = {'x': 1, 'y': 1, 'z':10} # dict
 print(vector_a, vector_b, vector_c)
+
+# Vector 
+u = [2, 2]
+v = [2, 3]
+z = [3, 5]
+result = [sum(t) for t in zip(u, v, z)]
+print(result)
+
+# Matrix
+
+# Matrix addition
+
+# Matrix product
