149197decab36707e055df1f9695878c7d0aae46
yh.hong <ghddpgml@gmail.com>
2024-03-18 15:55:16 +0900

    1.3

---
+++ 1-Pythonic Code/1.3-list-comprehension.py
@@ -0,0 +1,11 @@
+case_1 = ["A", "B", "C"]
+case_2 = ["D", "E", "F"]
+
+# one dimensional array
+result = [a+b for a in case_1 for b in case_2]
+print(result)
+
+# two dimensional array
+# 바깥 for문(b)이 고정됨
+result = [[a+b for a in case_1] for b in case_2]
+print(result)
